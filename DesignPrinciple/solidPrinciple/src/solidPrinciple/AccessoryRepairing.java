4740e97c844ff6db6dec9726d9fe07c7f3a4b14f
Sameeksha98 <51112223+Sameeksha98@users.noreply.github.com>
2021-03-22 17:01:16 +0530

    Add files via upload

---
+++ DesignPrinciple/solidPrinciple/src/solidPrinciple/AccessoryRepairing.java
@@ -0,0 +1,11 @@
+package solidPrinciple;
+
+public class AccessoryRepairing implements AccessoryRepair {
+
+	@Override
+	public void ProcessAccessoryRepair(String accessoryType) {
+		// TODO Auto-generated method stub
+		System.out.println(accessoryType + " repair accepted!!");
+	}
+
+}
